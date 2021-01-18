acecac8fe275582c86d329bd68f2e159e3d8f090
Reinier van der Hoeven <reinier.vanderhoeven@appythings.nl>
2021-01-18 16:47:51 +0100

    added access_tokens

--- src/models/apiproduct.js
+++ src/models/apiproduct.js
@@ -2,14 +2,15 @@ const Model = require('./model')
 
 module.exports = class ApiProduct extends Model {
   constructor (config) {
+    const auth = config.access_token ? { 'bearer': config.access_token } : {
+      user: config.username,
+      pass: config.password,
+    }
     super(
       `https://${config.host}/apiportal/api/1.0/Management.svc`,
       `https://${config.host}/apiportal/api/1.0/Management.svc`,
       'GET',
-      {
-        user: config.username,
-        pass: config.password
-      },
+      auth,
       'APIProducts'
     )
   }
